f27266030bc83365665a34758b373ce5d1ec6e9f
Malick FALL <emfall5015@gmail.com>
2026-05-20 18:38:20 +0200

    <Feat> Creation de l'API citation

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
-import requests
+# Importer la fonction get_citation de api_citation
 
+from api_citations import get_citation
 
-def get_citation():
-
-    url = "http://api.quotable.io/random"
-
-    response = requests.get(url)
-
-    # transforme le JSON en dictionnaire Python
-    data = response.json()
-
-    return data
-
-
-# récupération des données
 citation_data = get_citation()
-
-citation = citation_data["content"]
-auteur = citation_data["author"]
-
 
 # création du HTML dynamique
 html = f"""
@@ -28,64 +12,23 @@ html = f"""
 <head>
     <meta charset="UTF-8">
     <title>Mes citations</title>
-
-    <style>
-        body {{
-            font-family: Arial;
-            background-color: #f4f4f4;
-            text-align: center;
-            padding-top: 100px;
-        }}
-
-        .card {{
-            background: white;
-            width: 500px;
-            margin: auto;
-            padding: 30px;
-            border-radius: 10px;
-            box-shadow: 0 0 10px rgba(0,0,0,0.2);
-        }}
-
-        h1 {{
-            color: #6c2bd9;
-        }}
-
-        p {{
-            font-size: 20px;
-        }}
-
-        .author {{
-            margin-top: 20px;
-            font-weight: bold;
-        }}
-    </style>
-
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-
     <div class="card">
-
-        <h1>Citation du jour</h1>
-
-        <p>"{citation}"</p>
-
-        <div class="author">
-            - {auteur}
-        </div>
-
+    <h1>Citation du jour</h1>
+    <p>{citation_data["content"]}</p>
+    <div class="author">
+        {citation_data["author"]}
     </div>
-
+    </div>
 </body>
-
 </html>
 """
 
-
-# génération du fichier HTML
-with open("Nos_Citation.html", "w", encoding="utf-8") as file:
+# génération du page HTML
+with open("citation.html", "w", encoding="utf-8") as file:
     file.write(html)
 
 print("Fichier HTML créé avec succès !")
-
-
